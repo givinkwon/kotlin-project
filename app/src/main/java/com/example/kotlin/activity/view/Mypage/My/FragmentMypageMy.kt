1c8a1412d14299aee6ec14e45017f99052b54bd3
givinkwon <rlqls505@naver.com>
2022-01-17 20:12:36 +0900

    기눙 수정
    - scrollview size error(due to bottomview)
    - mypage my basic function

--- app/src/main/java/com/example/kotlin/activity/view/Mypage/My/FragmentMypageMy.kt
+++ app/src/main/java/com/example/kotlin/activity/view/Mypage/My/FragmentMypageMy.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin.activity.view.Mypage.My
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +9,8 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.kotlin.activity.view.Feed.FeedFeedRecyclerViewAdapter
 import com.example.kotlin.activity.data.viewmodel.FeedViewModel
+import com.example.kotlin.activity.data.viewmodel.UserViewModel
 import com.example.myapplication.databinding.FragmentMypageMyBinding
 import kotlinx.android.synthetic.main.fragment_mypage_my.*
 
@@ -17,8 +18,10 @@ class FragmentMypageMy : Fragment() {
     // RecyclerView
     // fragment_home.xml 연결 => lateinit => Fragment가 먼저 생성되고 선언될 수 있음 => onCreateView에서 binding 변수를 초기화함.
     private lateinit var binding: FragmentMypageMyBinding
+    private val userviewmodel by lazy { ViewModelProvider(this).get(UserViewModel::class.java) } // 생명주기 처리 없이 Livedata를 저장하고 있는 ViewModel
     private val feedviewmodel by lazy { ViewModelProvider(this).get(FeedViewModel::class.java) } // 생명주기 처리 없이 Livedata를 저장하고 있는 ViewModel
-    private lateinit var adapterfeed: FeedFeedRecyclerViewAdapter // 홀더에 데이터를 뿌려주는 Adapter
+    private lateinit var adapteruser: MypageMyUserRecyclerViewAdapter // 홀더에 데이터를 뿌려주는 Adapter
+    private lateinit var adapterfeed: MypageMyFeedRecyclerViewAdapter // 홀더에 데이터를 뿌려주는 Adapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,14 +43,14 @@ class FragmentMypageMy : Fragment() {
 
         // RecyclerView 시작
         // adapter init
-        adapterfeed = FeedFeedRecyclerViewAdapter(requireActivity())
+        adapterfeed = MypageMyFeedRecyclerViewAdapter(requireActivity())
 
         // recyclerview에 연결
         // 1. mypage_my_friend_recyclerview
         mypage_my_friend_recyclerView.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
 
         mypage_my_friend_recyclerView.adapter = adapterfeed
-        observerFeedData()
+
 
         // 2. higher_people_recyclerview
         mypage_my_higher_people_recyclerView.layoutManager = LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
@@ -60,7 +63,7 @@ class FragmentMypageMy : Fragment() {
 
         mypage_my_lower_people_recyclerView.adapter = adapterfeed
 
-
+        observerFeedData()
 
         // RecyclerView 끝
 
@@ -70,10 +73,20 @@ class FragmentMypageMy : Fragment() {
 // RecyclerView 시작
 
     // 데이터가 변화되었을 때 자동으로 데이터 가져온 후 변화 알려주기
+    fun observerUserData(){
+        userviewmodel.getUser() // 호출
+        userviewmodel.User.observe(requireActivity(), Observer {
+            adapteruser.setListData(it)
+            Log.d("data", it.email!!)
+        }) //LiveData observe
+    }
+
     fun observerFeedData(){
+        feedviewmodel.getFeed() // 호출
         feedviewmodel.Feed.observe(requireActivity(), Observer {
             adapterfeed.setListData(it)
-        })
+            Log.d("data", it.toString())
+        }) //LiveData observe
     }
 
     // RecyclerView 끝
